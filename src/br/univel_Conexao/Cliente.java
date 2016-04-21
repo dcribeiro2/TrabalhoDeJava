17f2ed63369acd0d75f5245d7c671688bf628c26
dcribeiro2 <dcribeiro2@WS-LAB02_124.univel.univel.br>
2016-04-20 21:57:42 -0300

    Commit Inicial

---
+++ src/br/univel_Conexao/Cliente.java
@@ -0,0 +1,23 @@
+package br.univel_Conexao;
+/*
+ * Classe Cliente contendo os atributos id,
+ * nome, endereço, telefone e estado civil, sendo que estado civil é 
+ * uma Enum que você deve criar. Ao armazenar esse campo no banco use seu valor ordinal.
+ */
+public class Cliente {
+	private int id;
+	private String nome;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+}
---
+++ src/br/univel_Conexao/Estado_Civil.java
@@ -0,0 +1,9 @@
+package br.univel_Conexao;
+
+public enum Estado_Civil {
+	Casado,
+	Solteiro,
+	Viuvo;
+	
+
+}
