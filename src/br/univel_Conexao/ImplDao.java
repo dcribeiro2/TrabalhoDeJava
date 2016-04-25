40cc75c7d50552f62a9e3d4c2f7b8218c79b6e6a
Diogo <diogo_sergio_ribeiro@hotmail.com>
2016-04-25 00:58:25 -0300

    Classe que implementa A classe Dao

---
+++ src/br/univel_Conexao/ImplDao.java
@@ -0,0 +1,31 @@
+package br.univel_Conexao;
+import java.util.List;
+
+public class ImplDao<K> implements Dao<K> {
+
+	@Override
+	public <T> void salvar(T t) {
+		
+	}
+
+	@Override
+	public <T> T buscar(K k) {
+		return null;
+	}
+
+	@Override
+	public <T> void atualizar(T t) {
+		
+	}
+
+	@Override
+	public void excluir(K k) {
+		
+	}
+
+	@Override
+	public <T> java.awt.List listarTodos() {
+		return null;
+	}
+
+}
