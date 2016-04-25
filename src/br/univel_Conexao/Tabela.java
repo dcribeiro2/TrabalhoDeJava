14a0fcfa625b025dde063d2e2a7427c392f64c04
Diogo <diogo_sergio_ribeiro@hotmail.com>
2016-04-24 23:58:38 -0300

    Mechendo na Annotation Tabela

--- src/br/univel_Conexao/Tabela.java
+++ src/br/univel_Conexao/Tabela.java
@@ -1,7 +1,15 @@
 package br.univel_Conexao;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
 
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.TYPE)
 public @interface Tabela {
+		
 	
-
+	String value() default "";
+	
 
 }
