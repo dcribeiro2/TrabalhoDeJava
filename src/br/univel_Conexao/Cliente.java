b9ae2bcb019bf55f535967be22801e436d145e95
Diogo <diogo_sergio_ribeiro@hotmail.com>
2016-04-24 22:28:56 -0300

    Mechendo em Cliente e coluna.

--- src/br/univel_Conexao/Cliente.java
+++ src/br/univel_Conexao/Cliente.java
@@ -5,25 +5,54 @@ package br.univel_Conexao;
  * uma Enum que você deve criar. Ao armazenar esse campo no banco use seu valor ordinal.
  */
 public class Cliente  {
-	
-	
-	
-	/*
-	 * 
-	 
+	@Coluna(pk=true, nome="idclifor")
 	private int id;
+	@Coluna(nome = "nomecliente", tamanho = 150 )
 	private String nome;
+	@Coluna(nome = "endereco", tamanho = 200)
+	private String endereco;
+	@Coluna(nome = "fone", tamanho = 10)
+	private String telefone;
+	@Coluna(nome = "estCivil")
+	private boolean estadocivil;
+
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-*/
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+
+	public boolean isEstadocivil() {
+		return estadocivil;
+	}
+
+	public void setEstadocivil(boolean estadocivil) {
+		this.estadocivil = estadocivil;
+	}
 }
--- src/br/univel_Conexao/Coluna.java
+++ src/br/univel_Conexao/Coluna.java
@@ -2,4 +2,9 @@ package br.univel_Conexao;
 
 public @interface Coluna {
 
+	String nome();
+	int tamanho() default -1;
+
+	boolean pk() default false;
+
 }
